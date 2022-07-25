b076c9de8f39602a07e9eb56f97bd5d6faaea5e2
sranvare07 <suraj@sixergame.com>
2022-07-25 18:47:55 +0530

    Overlay fixed

--- src/atoms/OverlayLoader.tsx
+++ src/atoms/OverlayLoader.tsx
@@ -8,7 +8,7 @@ type LoaderProps = {
 function OverlayLoader({ height, width }: LoaderProps) {
   return (
     <div
-      className={`w-screen h-screen absolute top-0 left-0 bottom-0 right-0 bg-slate-500 bg-opacity-80 text-white font-rajdhani font-medium flex justify-center items-center`}
+      className={`w-screen h-screen fixed top-0 left-0 bottom-0 right-0 bg-slate-500 bg-opacity-80 text-white font-rajdhani font-medium flex justify-center items-center`}
     >
       <div
         className={`w-1/3 h-1/2 bg-blue-400 rounded-md flex flex-col items-center justify-center border border-white`}
